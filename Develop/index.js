5782199a690c918a75da3f4e73dbf55f92f2c8c2
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-06-25 19:15:53 -0400

    writeFile function creates a basic .md

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,29 @@
+const fs = require("fs");
+
+
 // array of questions for user
 const questions = [
+        //npm inquirer documentation objects
 
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    //use the fs package
 }
 
 // function to initialize program
 function init() {
-
+    //use the inquirer package
 }
 
 // function call to initialize program
 init();
+
+fs.writeFile("newREADME.md", "# This is a readme file", function(err){
+    if (err) {
+        throw err;
+    }
+    console.log("Wrote the file");
+
+});
