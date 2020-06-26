c63a0cd4ae479d3c137c81442060dca0bdc81f92
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-06-26 10:04:54 -0400

    got rid of array for answers. answers are now added as parameters for writeToFile. writeToFile function is now working properly writing the correct values.

--- Develop/writefile.js
+++ Develop/writefile.js
@@ -2,15 +2,64 @@
 const fs = require("fs");
 
 //exports function for use in other js files
-
 module.exports = {
-    writeToFile: writeToFile
+    writeToFile: writeToFile,
 };
 
 // function to write README file
-function writeToFile() {
+function writeToFile(
+    title, 
+    description, 
+    installation, 
+    usage, 
+    license, 
+    contributing, 
+    test, 
+    question
+    ) {
     //use the fs package
-    fs.writeFile("newREADME.md", ``, "utf8", function(err){
+    fs.writeFile("newREADME.md", `
+
+# ${title}
+
+## Description 
+
+${description}
+
+
+## Table of Contents (Optional)
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+
+
+## Installation
+
+${installation}
+
+
+## Usage 
+
+
+
+## Credits
+
+
+
+## License
+
+
+
+## Contributing
+
+
+## Tests
+
+
+
+`, "utf8", function(err){
         if(err) {
             throw err;
         }
